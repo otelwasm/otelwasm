859c9faf38f089539cc05b77833d881c7ab20d30
Kotaro Inoue <k.musaino@gmail.com>
2025-04-04 00:51:14 +0900

    Add pdata
    Signed-off-by: Kotaro Inoue <k.musaino@gmail.com>

---
+++ pdata/proto/tools/tools.go
@@ -0,0 +1,28 @@
+// Read about tools here: https://github.com/golang/go/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
+
+//go:build tools
+
+/*
+   Copyright 2023 The Kubernetes Authors.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package tools
+
+// TODO: Replace this file with go tool introduced in later Go version
+
+import (
+	_ "github.com/knqyf263/go-plugin/cmd/protoc-gen-go-plugin"
+	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
+)
