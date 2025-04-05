b1c0639b2ae57d40657297e22fe31bf9f858db90
Kotaro Inoue <k.musaino@gmail.com>
2025-04-06 02:46:57 +0900

    Add path to config
    Signed-off-by: Kotaro Inoue <k.musaino@gmail.com>

--- wasmprocessor/config.go
+++ wasmprocessor/config.go
@@ -1,8 +1,14 @@
 package wasmprocessor
 
+import "fmt"
+
 type Config struct {
+	Path string `mapstructure:"path"`
 }
 
 func (cfg *Config) Validate() error {
+	if cfg.Path == "" {
+		return fmt.Errorf("path is required")
+	}
 	return nil
 }
