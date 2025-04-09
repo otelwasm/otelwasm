9fa665599c97a7c2d1593678ccbc349fd8f48c07
Kotaro Inoue <k.musaino@gmail.com>
2025-04-10 00:25:42 +0900

    Support metrics in nop processor example
    Signed-off-by: Kotaro Inoue <k.musaino@gmail.com>

--- examples/nop/main.go
+++ examples/nop/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/musaprg/otelwasm/guest/api"
 	"github.com/musaprg/otelwasm/guest/plugin" // register tracesprocessor
+	"go.opentelemetry.io/collector/pdata/pmetric"
 	"go.opentelemetry.io/collector/pdata/ptrace"
 )
 
@@ -11,11 +12,19 @@ func init() {
 }
 func main() {}
 
-var _ api.TracesProcessor = (*NopProcessor)(nil)
+var (
+	_ api.TracesProcessor  = (*NopProcessor)(nil)
+	_ api.MetricsProcessor = (*NopProcessor)(nil)
+)
 
 type NopProcessor struct{}
 
 // ProcessTraces implements api.TracesProcessor.
 func (n *NopProcessor) ProcessTraces(traces ptrace.Traces) (ptrace.Traces, *api.Status) {
 	return traces, nil
+}
+
+// ProcessMetrics implements api.MetricsProcessor.
+func (n *NopProcessor) ProcessMetrics(metrics pmetric.Metrics) (pmetric.Metrics, *api.Status) {
+	return metrics, nil
 }
