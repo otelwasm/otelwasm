d3fa33fd69c40fa06c76058bfd0235cb738a02c3
Kotaro Inoue <k.musaino@gmail.com>
2025-04-18 02:11:39 +0900

    Add stdout exporter example
    Signed-off-by: Kotaro Inoue <k.musaino@gmail.com>

---
+++ examples/exporter/stdout/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/musaprg/otelwasm/guest/api"
+	"github.com/musaprg/otelwasm/guest/plugin" // register tracesexporter, metricsexporter, logsexporter
+	"go.opentelemetry.io/collector/pdata/plog"
+	"go.opentelemetry.io/collector/pdata/pmetric"
+	"go.opentelemetry.io/collector/pdata/ptrace"
+)
+
+func init() {
+	plugin.Set(&StdoutExporter{})
+}
+func main() {}
+
+var (
+	_ api.TracesExporter  = (*StdoutExporter)(nil)
+	_ api.MetricsExporter = (*StdoutExporter)(nil)
+	_ api.LogsExporter    = (*StdoutExporter)(nil)
+)
+
+type StdoutExporter struct{}
+
+// PushTraces implements api.TracesExporter.
+func (e *StdoutExporter) PushTraces(traces ptrace.Traces) *api.Status {
+	marshaler := ptrace.JSONMarshaler{}
+	jsonData, err := marshaler.MarshalTraces(traces)
+	if err != nil {
+		return &api.Status{
+			Code:   api.StatusCodeError,
+			Reason: fmt.Sprintf("failed to marshal traces to JSON: %v", err),
+		}
+	}
+
+	fmt.Println(string(jsonData))
+	return nil
+}
+
+// PushMetrics implements api.MetricsExporter.
+func (e *StdoutExporter) PushMetrics(metrics pmetric.Metrics) *api.Status {
+	marshaler := pmetric.JSONMarshaler{}
+	jsonData, err := marshaler.MarshalMetrics(metrics)
+	if err != nil {
+		return &api.Status{
+			Code:   api.StatusCodeError,
+			Reason: fmt.Sprintf("failed to marshal metrics to JSON: %v", err),
+		}
+	}
+
+	fmt.Println(string(jsonData))
+	return nil
+}
+
+// PushLogs implements api.LogsExporter.
+func (e *StdoutExporter) PushLogs(logs plog.Logs) *api.Status {
+	marshaler := plog.JSONMarshaler{}
+	jsonData, err := marshaler.MarshalLogs(logs)
+	if err != nil {
+		return &api.Status{
+			Code:   api.StatusCodeError,
+			Reason: fmt.Sprintf("failed to marshal logs to JSON: %v", err),
+		}
+	}
+
+	fmt.Println(string(jsonData))
+	return nil
+}
