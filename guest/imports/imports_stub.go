47c651e9cad1dfc3d7b159bef31e8f28828df7cc
Tsuzu <8574909+tsuzu@users.noreply.github.com>
2025-04-21 00:25:19 +0900

    Fix a build error

--- guest/imports/imports_stub.go
+++ guest/imports/imports_stub.go
@@ -6,8 +6,8 @@ package imports
 
 func getPluginConfig(ptr, size uint32) (len uint32) { return }
 
-func setResultTraces(ptr, size uint32)
+func setResultTraces(ptr, size uint32) { return }
 
-func setResultMetrics(ptr, size uint32)
+func setResultMetrics(ptr, size uint32) { return }
 
-func setResultLogs(ptr, size uint32)
+func setResultLogs(ptr, size uint32) { return }
