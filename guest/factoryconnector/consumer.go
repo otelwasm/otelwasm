bafda2a2247fca2169be7e12d1ff320e39756ac3
Tsuzu <8574909+tsuzu@users.noreply.github.com>
2025-05-02 22:16:01 +0900

    Introduce factoryconnector

---
+++ guest/factoryconnector/consumer.go
@@ -0,0 +1,32 @@
+package factoryconnector
+
+import (
+	"context"
+
+	"github.com/otelwasm/otelwasm/guest/imports"
+	"go.opentelemetry.io/collector/consumer"
+	"go.opentelemetry.io/collector/pdata/plog"
+	"go.opentelemetry.io/collector/pdata/pmetric"
+	"go.opentelemetry.io/collector/pdata/ptrace"
+)
+
+var _ consumer.ConsumeLogsFunc = ConsumeLogs
+
+func ConsumeLogs(ctx context.Context, ld plog.Logs) error {
+	imports.SetResultLogs(ld)
+	return nil
+}
+
+var _ consumer.ConsumeMetricsFunc = ConsumeMetrics
+
+func ConsumeMetrics(ctx context.Context, ld pmetric.Metrics) error {
+	imports.SetResultMetrics(ld)
+	return nil
+}
+
+var _ consumer.ConsumeTracesFunc = ConsumeTraces
+
+func ConsumeTraces(ctx context.Context, ld ptrace.Traces) error {
+	imports.SetResultTraces(ld)
+	return nil
+}
