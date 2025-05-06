0c0c3d0ec6a74a458c2d7c3b93e7264b414d2756
Kotaro Inoue <k.musaino@gmail.com>
2025-05-07 01:45:59 +0900

    Add keepalive of objects in Consume* function
    Signed-off-by: Kotaro Inoue <k.musaino@gmail.com>

--- guest/factoryconnector/consumer.go
+++ guest/factoryconnector/consumer.go
@@ -2,6 +2,7 @@ package factoryconnector
 
 import (
 	"context"
+	"runtime"
 
 	"github.com/otelwasm/otelwasm/guest/imports"
 	"go.opentelemetry.io/collector/consumer"
@@ -14,6 +15,7 @@ var _ consumer.ConsumeLogsFunc = ConsumeLogs
 
 func ConsumeLogs(ctx context.Context, ld plog.Logs) error {
 	imports.SetResultLogs(ld)
+	runtime.KeepAlive(ld) // until ptr is no longer needed.
 	return nil
 }
 
@@ -21,6 +23,7 @@ var _ consumer.ConsumeMetricsFunc = ConsumeMetrics
 
 func ConsumeMetrics(ctx context.Context, ld pmetric.Metrics) error {
 	imports.SetResultMetrics(ld)
+	runtime.KeepAlive(ld) // until ptr is no longer needed.
 	return nil
 }
 
@@ -28,5 +31,6 @@ var _ consumer.ConsumeTracesFunc = ConsumeTraces
 
 func ConsumeTraces(ctx context.Context, ld ptrace.Traces) error {
 	imports.SetResultTraces(ld)
+	runtime.KeepAlive(ld) // until ptr is no longer needed.
 	return nil
 }
