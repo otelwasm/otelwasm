d07c8d197d910700b92a7cf814c34be42f24185e
Kotaro Inoue <k.musaino@gmail.com>
2025-05-07 00:36:58 +0900

    Use upstream otlphttpexporter directly
    Signed-off-by: Kotaro Inoue <k.musaino@gmail.com>

--- examples/exporter/otlphttpexporter/main.go
+++ examples/exporter/otlphttpexporter/main.go
@@ -1,46 +1,15 @@
 package main
 
 import (
-	"context"
-
-	"github.com/otelwasm/otelwasm/examples/exporter/otlphttpexporter/otlphttpexporter"
 	"github.com/otelwasm/otelwasm/guest/api"
 	"github.com/otelwasm/otelwasm/guest/factoryconnector"
 	"github.com/otelwasm/otelwasm/guest/plugin" // register exporters
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/component/componenttest"
 	"go.opentelemetry.io/collector/exporter"
-	"go.opentelemetry.io/collector/pdata/ptrace"
+	"go.opentelemetry.io/collector/exporter/otlphttpexporter"
 	"go.uber.org/zap"
 )
-
-var _ api.TracesExporter = (*exp)(nil)
-
-type exp struct {
-	e exporter.Traces
-}
-
-func (e exp) PushTraces(td ptrace.Traces) *api.Status {
-	if e.e == nil {
-		return &api.Status{
-			Code:   api.StatusCodeError,
-			Reason: "exporter is not initialized",
-		}
-	}
-
-	ctx := context.Background()
-	err := e.e.ConsumeTraces(ctx, td)
-	if err != nil {
-		return &api.Status{
-			Code:   api.StatusCodeError,
-			Reason: err.Error(),
-		}
-	}
-
-	return &api.Status{
-		Code: api.StatusCodeSuccess,
-	}
-}
 
 func init() {
 	logger, err := zap.NewDevelopment()
@@ -69,22 +38,6 @@ func init() {
 		connector.Logs(),
 		connector.Traces(),
 	})
-
-	// e, err := factory.CreateTraces(context.Background(), settings, &otlphttpexporter.Config{
-	// 	ClientConfig: confighttp.ClientConfig{Endpoint: "http://localhost:4319"},
-	// 	Encoding:     otlphttpexporter.EncodingProto,
-	// })
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// if err := e.Start(context.Background(), componenttest.NewNopHost()); err != nil {
-	// 	panic(err)
-	// }
-
-	// plugin.Set(exp{
-	// 	e: e,
-	// })
 }
 
 func main() {}
