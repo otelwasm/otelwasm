e844b4f56c801c5c9b73b72da0459f97878e34bc
Kotaro Inoue <k.musaino@gmail.com>
2025-05-08 00:58:07 +0900

    Add benchmark with compiled mode
    Signed-off-by: Kotaro Inoue <k.musaino@gmail.com>

--- wasmprocessor/benchmark_test.go
+++ wasmprocessor/benchmark_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/otelwasm/otelwasm/wasmplugin"
 	"go.opentelemetry.io/collector/component/componenttest"
 	"go.opentelemetry.io/collector/consumer/consumertest"
 	"go.opentelemetry.io/collector/pdata/pcommon"
@@ -24,10 +25,47 @@ func generateExampleTraces() ptrace.Traces {
 	return td
 }
 
-func BenchmarkNopProcessorWasm(b *testing.B) {
+func BenchmarkNopProcessorWasmInterpreter(b *testing.B) {
 	// Test that the processor can be created with the default config
 	factory := NewFactory()
 	cfg := factory.CreateDefaultConfig().(*Config)
+	cfg.Path = "testdata/nop/main.wasm"
+	ctx := b.Context()
+
+	// Test for traces
+	settings := processortest.NewNopSettings(typeStr)
+	tp, err := factory.CreateTraces(ctx, settings, cfg, consumertest.NewNop())
+	if err != nil {
+		b.Fatalf("failed to create traces processor: %v", err)
+	}
+	if tp == nil {
+		b.Fatal("traces processor is nil")
+	}
+
+	if err := tp.Start(ctx, componenttest.NewNopHost()); err != nil {
+		b.Errorf("failed to start processor: %v", err)
+	}
+
+	td := generateExampleTraces()
+
+	b.Run("process traces", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			if err := tp.ConsumeTraces(ctx, td); err != nil {
+				b.Errorf("failed to consume traces: %v", err)
+			}
+		}
+	})
+
+	if err := tp.Shutdown(ctx); err != nil {
+		b.Errorf("failed to shutdown processor: %v", err)
+	}
+}
+
+func BenchmarkNopProcessorWasmCompiled(b *testing.B) {
+	// Test that the processor can be created with the default config
+	factory := NewFactory()
+	cfg := factory.CreateDefaultConfig().(*Config)
+	cfg.RuntimeConfig.Mode = wasmplugin.RuntimeModeCompiled
 	cfg.Path = "testdata/nop/main.wasm"
 	ctx := b.Context()
 
